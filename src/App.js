452be3bbb38e82701450d1b5dcdbf15c50d48680
0Anshu1 <anshudu2004@gmail.com>
2024-07-26 10:04:20 +0530

    MERN APP -2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
 
 function App() {
+  const { isAuthenticated, user, logout } = useAuth0();
+
+  if (!isAuthenticated) {
+    // If not authenticated, show a loading message or redirect to the login page
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1>Great!! You Are now logged in!!</h1>
+        <p>Welcome, {user.name}!</p>
+        <button
+          onClick={() => 
+            logout({ returnTo: 'http://localhost:3000' }) // Redirect to app1
+          }
         >
-          Learn React
-        </a>
+          Log Out
+        </button>
       </header>
     </div>
   );
